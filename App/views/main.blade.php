7428f791fcecb8c0bee4a2bc7fc27f0bec8da9f9
HyeN123 <tiqmffk123@gmail.com>
2019-06-01 12:21:35 +0900

    초기

---
+++ App/views/main.blade.php
@@ -0,0 +1,15 @@
+@extends("layout/master")
+
+@section('maincontent')
+    <section id="slider"> <!-- 슬라이더 섹션 -->
+        <div class="container">
+            슬라이드 섹션
+        </div>
+    </section>
+
+    <div class="container"> <!-- 에디터 픽 섹션 -->
+        <section id="editorPick">
+            에디터 픽 섹션
+        </section>
+    </div>
+@endsection
---
+++ index.js
@@ -0,0 +1,4 @@
+import $ from 'jquery';
+
+import './style.css';
+import './node_modules/bootstrap/dist/css/bootstrap.min.css'
